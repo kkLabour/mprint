329f597abdc40ce108cb486d2d4a1d83f896e3bd
morris.dpc <morris.dpc@gmail.com>
2011-03-19 13:28:25 +0000

    0.0.6 - added the property 'alt' (string), which is the text that will appear if the image is not available - modified the way the property is being checked from isset() to strlen(). - enhanced the way in producing the image tag and used the Yii-way instead of manually creating it. - enhanced (somehow) the documentation.  * 0.0.5 - added the property 'showIcon' (bool), which tells whether to include the image in the link or not - added the property 'image' (string), which tells the widget what icon to use. With this, different icons can be used in different widget calls (for example, one grid icon to print the datagrid results, and one calendar icon to print the calendar div). It will only be rendered if the properties 'visible' and 'showIcon' are set to 'true'. Default value is 'printer.png'  * 0.0.4 - added the property 'visible' (bool), which dicates the visibility of the printing link - modified some lines of codes to make it more clearer  * 0.0.3 - fixed the way the JS file creates the iFrame (the <head> is inside the <body>) - added the execCommand in mPrint.js to fix IE7 and IE8 issue on printing banners

--- assets/mPrint.js
+++ assets/mPrint.js
@@ -27,88 +27,130 @@
 
 // Create a jquery plugin that prints the given element.
 jQuery.fn.print = function(documentName,cssFile){
-	// NOTE: We are trimming the jQuery collection down to the
-	// first element in the collection.
-	if (this.size() > 1){
-		this.eq( 0 ).print();
-		return;
-	} else if (!this.size()){
-		return;
-	}
+    // NOTE: We are trimming the jQuery collection down to the
+    // first element in the collection.
+    if (this.size() > 1){
+        this.eq( 0 ).print();
+        return;
+    } else if (!this.size()){
+        return;
+    }
  
-	// ASSERT: At this point, we know that the current jQuery
-	// collection (as defined by THIS), contains only one
-	// printable element.
+    // ASSERT: At this point, we know that the current jQuery
+    // collection (as defined by THIS), contains only one
+    // printable element.
  
-	// Create a random name for the print frame.
-	var strFrameName = ("printer-" + (new Date()).getTime());
+    // Create a random name for the print frame.
+    var strFrameName = ("printer-" + (new Date()).getTime());
  
-	// Create an iFrame with the new name.
-	var jFrame = $( "<iframe name='" + strFrameName + "'>" );
+    // Create an iFrame with the new name.
+    var jFrame = $( "<iframe name='" + strFrameName + "'>" );
  
-	// Hide the frame (sort of) and attach to the body.
-	jFrame
-		.css( "width", "1px" )
-		.css( "height", "1px" )
-		.css( "position", "absolute" )
-		.css( "left", "-9999px" )
-		.appendTo( $( "body:first" ) )
-	;
+    // Hide the frame (sort of) and attach to the body.
+    jFrame
+    .css( "width", "1px" )
+    .css( "height", "1px" )
+    .css( "position", "absolute" )
+    .css( "left", "-9999px" )
+    .appendTo( $( "body:first" ) )
+    ;
  
-	// Get a FRAMES reference to the new frame.
-	var objFrame = window.frames[ strFrameName ];
+    // Get a FRAMES reference to the new frame.
+    var objFrame = window.frames[ strFrameName ];
  
-	// Get a reference to the DOM in the new frame.
-	var objDoc = objFrame.document;
+    // Get a reference to the DOM in the new frame.
+    var objDoc = objFrame.document;
  
-	// Grab all the style tags and copy to the new
-	// document so that we capture look and feel of
-	// the current document.
+    // Grab all the style tags and copy to the new
+    // document so that we capture look and feel of
+    // the current document.
  
-	// Create a temp document DIV to hold the style tags.
-	// This is the only way I could find to get the style
-	// tags into IE.
-	var jStyleDiv = $( "<div>" ).append(
-		$( "style" ).clone()
-		);
- 
-	// Write the HTML for the document. In this, we will
-	// write out the HTML of the current element.
-	objDoc.open();
-	objDoc.write( "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" );
-	objDoc.write( "<html>" );
-	objDoc.write( "<body>" );
-	objDoc.write( "<head>" );
-	objDoc.write( "<title>" );
+    // Create a temp document DIV to hold the style tags.
+    // This is the only way I could find to get the style
+    // tags into IE.
+    var jStyleDiv = $( "<div>" ).append(
+        $( "style" ).clone()
+        );
+
+    /**
+     * Write the contents in a variable first then print them all at once
+     * As described by Jaxley @http://www.bennadel.com/blog/1591-Ask-Ben-Print-Part-Of-A-Web-Page-With-jQuery.htm
+     * @quote "FYI, you have some bugs in your HTML markup. You can't put the head tag within the body tag.
+     * Also, writing is fairly slow in general so you could optimize your html
+     * by creating a string first and writing once instead of multiple writes."
+     *
+     * I think it is also great to use html4 instead of xhtml for older browsers. What do you think?
+     */    
+    var printContents = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">" +
+        "<html>" +
+        "<head><title>" + documentName + "</title>" +
+        jStyleDiv.html() +
+        '<link rel="stylesheet" type="text/css" href="'+cssFile+'" />' +
+        "</head>" +
+        "<body>" +
+        this.html() +
+        "</body></html>";
+
+    // Write the HTML for the document. In this, we will
+    // write out the HTML of the current element.
+    objDoc.open();
+    objDoc.write(printContents);
+    objDoc.close();
     
     /**
-     *This is the original line, which I replaced by 'documentName'
-     *objDoc.write( document.title );
-     */
-	objDoc.write( documentName );
-
-	objDoc.write( "</title>" );
-	objDoc.write( jStyleDiv.html() );
-
-    //this line is added
-	objDoc.write( '<link rel="stylesheet" type="text/css" href="'+cssFile+'" />' );
+     * This is the original content. Feel free to uncomment this one and comment out
+     * the codes above.
+     **/
+    /*
+    // Write the HTML for the document. In this, we will
+    // write out the HTML of the current element.
+    objDoc.open();
+    objDoc.write( "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" );
+    objDoc.write( "<html>" );
+    objDoc.write( "<body>" );
+    objDoc.write( "<head>" );
+    objDoc.write( "<title>" );
+    objDoc.write( document.title );
+    objDoc.write( "</title>" );
+    objDoc.write( jStyleDiv.html() );objDoc.write( "</head>" );
+    objDoc.write( this.html() );
+    objDoc.write( "</body>" );
+    objDoc.write( "</html>" );
     
-	objDoc.write( "</head>" );
-	objDoc.write( this.html() );
-	objDoc.write( "</body>" );
-	objDoc.write( "</html>" );
-	objDoc.close();
+    objDoc.close();
+    */
+
+   
+    // Print the document.
+    objFrame.focus();
+
+    /**
+     * Added on March 18, 2011
+     *
+     * I've been scrolling down and reading the comments now regarding the script,
+     * and this is one of the improvements Douglas has pointed out so as to address
+     * the issue when printing flash content in IE 7/8,wherein they are being scaled down.
+     */
+    try {
+        var exec = objDoc.execCommand('print',false,null);
+    }
+    catch(err)    {
+    //catch nothing
+    }
+    if(exec!=true)    {
+        objFrame.print();
+    }
+    objFrame.print();
  
-	// Print the document.
-	objFrame.focus();
-	objFrame.print();
- 
-	// Have the frame remove itself in about a minute so that
-	// we don't build up too many of these frames.
-	setTimeout(
-		function(){
-			jFrame.remove();
-		},
-		(60 * 1000)
-		);
+    // Have the frame remove itself in about a minute so that
+    // we don't build up too many of these frames.
+    /**
+     * Made the frame disappear from one minute to 15 seconds
+     */
+    setTimeout(
+        function(){
+            jFrame.remove();
+        },
+        (160 * 1000)
+    );
 }   
