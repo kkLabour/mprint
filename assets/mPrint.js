f1c7f23fa707bbec275539e5d0da213ea16eb3bc
macinville <macinville@gmail.com>
2011-03-22 16:25:46 +0000

    1.0   - added the property 'cssFile' which is intended to replace 'css' so as to avoid confusion for those       who are used to in cssFile (besides, it is more definitive since it is a property for a css file)   - added property 'dbgHeight' and 'dbgWidth', which will be the size of the debug portion   - added the property 'timeOut' which will tell how many seconds before the iframe disappears. This       will only be usable if debug is set to false   - added the method mCssFile which will manage what value to use between css and cssFile property   - made some adjustments with the js  0.1.0   - added the variable 'debug' so that the result can be seen and checked first before       sending it out to the printer   - enhanced the passsing of parameters to the js by using json_encode function   - enhanced the js code (added $.fn.print.defaults)   - fixed some bugs found when using FF

--- assets/mPrint.js
+++ assets/mPrint.js
@@ -19,58 +19,66 @@
 
 /**
  * Edited by: Morris Jencen O. Chavez <macinville@gmail.com>
- * March 18, 2011
- * Added the parameter 'documentName' which will serve as, well, the name
- * of the document to be printed.
- * Added the parameter 'cssFile' which will serve as the CSS for the file to be printed
  */
+(function($) {
+    var mac;
 
-// Create a jquery plugin that prints the given element.
-jQuery.fn.print = function(documentName,cssFile){
-    // NOTE: We are trimming the jQuery collection down to the
-    // first element in the collection.
-    if (this.size() > 1){
-        this.eq( 0 ).print();
-        return;
-    } else if (!this.size()){
-        return;
-    }
+    // Create a jquery plugin that prints the given element.
+    jQuery.fn.print = function(options){
+        mac = $.extend({}, $.fn.print.defaults, options);
+        // NOTE: We are trimming the jQuery collection down to the
+        // first element in the collection.
+        if (this.size() > 1){
+            this.eq( 0 ).print();
+            return;
+        } else if (!this.size()){
+            return;
+        }
  
-    // ASSERT: At this point, we know that the current jQuery
-    // collection (as defined by THIS), contains only one
-    // printable element.
+        // ASSERT: At this point, we know that the current jQuery
+        // collection (as defined by THIS), contains only one
+        // printable element.
  
-    // Create a random name for the print frame.
-    var strFrameName = ("printer-" + (new Date()).getTime());
+        // Create a random name for the print frame.
+        var strFrameName = ("printer-" + (new Date()).getTime());
  
-    // Create an iFrame with the new name.
-    var jFrame = $( "<iframe name='" + strFrameName + "'>" );
+        // Create an iFrame with the new name.
+        var jFrame = $( "<iframe name='" + strFrameName + "'>" );
  
-    // Hide the frame (sort of) and attach to the body.
-    jFrame
-    .css( "width", "1px" )
-    .css( "height", "1px" )
-    .css( "position", "absolute" )
-    .css( "left", "-9999px" )
-    .appendTo( $( "body:first" ) )
-    ;
+        // Hide the frame (sort of) and attach to the body.
+        if(!mac.debug)   {
+            jFrame
+            .css( "width", "0px" )
+            .css( "height", "0px" )
+            .css( "position", "absolute" )
+            .css( "left", "-9999px" )
+            .css( "top", "-9999px" );
+        }
+        else    {
+            jFrame
+            .css( "width", mac.dbgWidth )
+            .css( "height", mac.dbgHeight )
+            .css( "position", "absolute" )
+        }
+
+        jFrame.appendTo( $( "body:first" ) );
  
-    // Get a FRAMES reference to the new frame.
-    var objFrame = window.frames[ strFrameName ];
+        // Get a FRAMES reference to the new frame.
+        var objFrame = window.frames[ strFrameName ];
  
-    // Get a reference to the DOM in the new frame.
-    var objDoc = objFrame.document;
+        // Get a reference to the DOM in the new frame.
+        var objDoc = objFrame.document;
  
-    // Grab all the style tags and copy to the new
-    // document so that we capture look and feel of
-    // the current document.
+        // Grab all the style tags and copy to the new
+        // document so that we capture look and feel of
+        // the current document.
  
-    // Create a temp document DIV to hold the style tags.
-    // This is the only way I could find to get the style
-    // tags into IE.
-    var jStyleDiv = $( "<div>" ).append(
-        $( "style" ).clone()
-        );
+        // Create a temp document DIV to hold the style tags.
+        // This is the only way I could find to get the style
+        // tags into IE.
+        var jStyleDiv = $( "<div>" ).append(
+            $( "style" ).clone()
+            );
 
     /**
      * Write the contents in a variable first then print them all at once
@@ -81,76 +89,60 @@ jQuery.fn.print = function(documentName,cssFile){
      *
      * I think it is also great to use html4 instead of xhtml for older browsers. What do you think?
      */    
-    var printContents = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">" +
-        "<html>" +
-        "<head><title>" + documentName + "</title>" +
-        jStyleDiv.html() +
-        '<link rel="stylesheet" type="text/css" href="'+cssFile+'" />' +
-        "</head>" +
-        "<body>" +
-        this.html() +
-        "</body></html>";
+        var printContents = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">" +
+            "<html>" +
+            "<head><title>" + mac.documentName + "</title>" +
+            jStyleDiv.html() +
+            '<link rel="stylesheet" type="text/css" href="'+mac.cssFile+'" />' +
+            "</head>" +
+            "<body>" +
+            this.html() +
+            "</body></html>";
 
-    // Write the HTML for the document. In this, we will
-    // write out the HTML of the current element.
-    objDoc.open();
-    objDoc.write(printContents);
-    objDoc.close();
+        // Write the HTML for the document. In this, we will
+        // write out the HTML of the current element.
+        objDoc.open();
+        objDoc.write(printContents);
+        objDoc.close(); 
     
-    /**
-     * This is the original content. Feel free to uncomment this one and comment out
-     * the codes above.
-     **/
-    /*
-    // Write the HTML for the document. In this, we will
-    // write out the HTML of the current element.
-    objDoc.open();
-    objDoc.write( "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" );
-    objDoc.write( "<html>" );
-    objDoc.write( "<body>" );
-    objDoc.write( "<head>" );
-    objDoc.write( "<title>" );
-    objDoc.write( document.title );
-    objDoc.write( "</title>" );
-    objDoc.write( jStyleDiv.html() );objDoc.write( "</head>" );
-    objDoc.write( this.html() );
-    objDoc.write( "</body>" );
-    objDoc.write( "</html>" );
-    
-    objDoc.close();
-    */
-
    
-    // Print the document.
-    objFrame.focus();
+        // Print the document.
+        objFrame.focus();
 
-    /**
+        /**
      * Added on March 18, 2011
      *
      * I've been scrolling down and reading the comments now regarding the script,
      * and this is one of the improvements Douglas has pointed out so as to address
      * the issue when printing flash content in IE 7/8,wherein they are being scaled down.
      */
-    try {
-        var exec = objDoc.execCommand('print',false,null);
-    }
-    catch(err)    {
-    //catch nothing
-    }
-    if(exec!=true)    {
-        objFrame.print();
-    }
-    objFrame.print();
+        try {
+            var exec = objDoc.execCommand('print',false,null);
+        }
+        catch(err)    {
+        //catch nothing
+        }
+        if(exec!=true && !mac.debug)    {
+            objFrame.print();
+        }
  
-    // Have the frame remove itself in about a minute so that
-    // we don't build up too many of these frames.
-    /**
-     * Made the frame disappear from one minute to 15 seconds
-     */
-    setTimeout(
-        function(){
-            jFrame.remove();
-        },
-        (160 * 1000)
-    );
-}   
+        // Have the frame remove itself in about a minute so that
+        // we don't build up too many of these frames.
+        if(!mac.debug) {
+            setTimeout(
+                function(){
+                    jFrame.remove();
+                },
+                (mac.timeOut * 1000)
+                );
+        }
+        $.fn.print.defaults = {
+            debug: false,
+            dbgHeight: '100%',
+            dbgWidtht: '100%',
+            cssFile: '',
+            documentName : document.title,
+            timeOut: 60
+        };
+    }
+})(jQuery);
